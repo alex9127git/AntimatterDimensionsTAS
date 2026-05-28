8daade8ab39c318922ede562b29ad81dc8a5be51
Jade <jadeispwiki@gmail.com>
2026-05-27 20:25:51 -0700

    Logic cleaning--see tas.js comments for more information.

--- src/tas.js
+++ src/tas.js
@@ -1,5 +1,21 @@
 import { buyOneDimension, buyTickSpeed, requestDimensionBoost } from "./core/globals"
 
+/*  Addition notes (from Jade :3):
+*-- We can add a return true for functions that don't 
+*-- have an implicit return statement,
+*
+*-- Cleaned up some of the logic, mainly:
+*-- -- logic regarding creating the instructions
+*-- -- especially as they are stored.
+*-- -- See runNextPendingInstruction;
+*
+*-- The current problem is that
+*-- -- requestDimensionBoost();
+*-- -- doesn't return false/true, only undefined.
+*-- -- this is annoying, we can either just force 
+*-- -- it to return a boolean, or change our checks.
+*
+*/
 export const actions = {
     ["buyOneDimension"]: buyOneDimension,
     ["buyTickSpeed"]: buyTickSpeed,
@@ -10,6 +26,7 @@ export const actions = {
 export const TAS = {
     isRunning: false,
     nextTickSwitch: true,
+
     instructions: [
         createInstruction(() => simulateEvent("keydown", "up", 38)),
         createInstruction(() => waitNextTick()),
@@ -44,83 +61,102 @@ export const TAS = {
         createInstruction(() => buyOneDimension(1)),
         createInstruction(() => buyOneDimension(2))
     ],
+
     currentInstruction: 0,
     startTime: null,
     queue: [],
+
     enable() {
         console.log("TAS started running");
         this.isRunning = true;
         this.nextTickSwitch = true;
         this.currentInstruction = 0;
         this.runNextPendingInstruction();
+        return true;
     },
 
+    // I'm scared to add a return 
+    // statement on this behemoth of a function
     runNextPendingInstruction() {
         if (!this.isRunning) return;
         let isSuccessful = true;
         while (isSuccessful) {
             isSuccessful = this.runOneInstruction(this.currentInstruction);
-            if (isSuccessful && (this.currentInstruction === 21 || this.currentInstruction > 22)) {
-            this.startTime = this.startTime || performance.now();
-		    console.log(`
-			    Bought at: ${performance.now() - this.startTime}ms,
-			    step: ${this.currentInstruction}`);
-	    }
-	    if (isSuccessful)  this.currentInstruction += 1;
-        }
+
+            if (isSuccessful) {
+                if (this.currentInstruction === 21 || this.currentInstruction > 22) {
+
+                this.startTime = this.startTime || performance.now();
+    
+		        console.log(`
+			        Bought at: ${performance.now() - this.startTime}ms,
+			        step: ${this.currentInstruction}`);
+
+	            };
+	        this.currentInstruction += 1;
+            };
+        };
     },
 
     runOneInstruction(index) {
         if (index >= this.instructions.length) {
             TAS.isRunning = false;
-            console.log("TAS finished running");
-            return;
-        }
+        // we can just return the console.log statement,
+        // this is still fine for readability.
+            return console.log("TAS finished running");;
+        };
+        
         let instruction = this.instructions[index];
         return instruction.run();
     },
+
     restart(name = "JadeGPTas") {
+
+        // reset the TAS values.
         TAS.startTime = null;
     	TAS.isRunning = false;
     	TAS.currentInstruction = 0;
-    	dev.hardReset();
-    	Speedrun.prepareSave(name);
+
+    	dev.hardReset(); // reset the achievements
+    	Speedrun.prepareSave(name); // enable speedrun mode
+        return true;
     },
+
     async getInstructions(path) {
         const response = await fetch(path);
         const text = await response.text();
         const data = JSON.parse(text);
-
         const instructions = [];
+
         data.forEach(([fn, args]) => {
             instructions.push(createInstruction(() => actions[fn](...args)));
         });   
         this.queue = instructions;
+        return true;
     },
-    async loadInstructions(path) {
-        if (this.queue.length === 0) {
-            await this.getInstructions(path);
-        } else {
-            this.instructions = this.queue;
-        };
+
+    loadInstructions() {
+        this.instructions = this.queue;
+        return true;
     }
-}
+};
 
 export function tasTick() {
     TAS.runNextPendingInstruction();
-}
+    return true;
+};
 
 export function waitNextTick() {
     TAS.nextTickSwitch = !TAS.nextTickSwitch;
     return TAS.nextTickSwitch;
-}
+};
 
 export function createInstruction(action) {
     return {
         action: action,
         run: (() => action())
     };
-}
+};
 
 export function simulateEvent(type, key, keyCode) {
     const event = new KeyboardEvent(type, {
@@ -129,4 +165,4 @@ export function simulateEvent(type, key, keyCode) {
         which: keyCode
     });
     return document.dispatchEvent(event);
-}
+};
